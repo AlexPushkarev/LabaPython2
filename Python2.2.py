b11f074ffe12e5fc9efac65d3cb239fadd3fcadc
AlexPushkarev <47695425+AlexPushkarev@users.noreply.github.com>
2019-04-02 14:35:00 +0300

    Add files via upload

--- Python2.2.py
+++ Python2.2.py
@@ -1,28 +1,33 @@
 s = input()
 
-print('      ФИО', '                  ', 'О студенте')
+print('     ФИО', end=' ')
+print('О студенте'.rjust(45))
 
-a1 = s.find('Иванов')
-s1 = s[a1:a1+6]
-s2 = s[a1+7:a1+11]
-a1 = s.find('Иванович')
-s3 = s[a1:a1+8]
-a1 = s.find('Студент')
-s5 = s[a1:a1+15]
-a1 = s.find('23')
-s6 = s[a1:a1+7]
-stroka_1 = s1 + ' ' + s2 + ' ' + s3 + '   ' + s5 + ',' + s6
-print(stroka_1)
+list1 = s.split('_')
+s1 = ''
 
-a1 = s.find('Петров')
-s1 = s[a1:a1+6]
-a1 = s.find('Семен')
-s2 = s[a1:a1+5]
-a1 = s.find('Игоревич')
-s3 = s[a1:a1+8]
-a1 = s.rfind('Студент')
-s5 = s[a1:a1+15]
-a1 = s.find('22')
-s6 = s[a1:a1+7]
-stroka_2 = s1 + ' ' + s2 + ' ' + s3 + '  ' + s5 + ',' + s6
-print(stroka_2)
+count = 0
+
+for i in range(1,  len(list1)):
+    count = 0
+    list2 = list1[i].split(';')
+    for j in list2:
+        count = count + 1
+        s1 = str(j)
+        if count < 4:
+            if count == 3:
+                if i == 2:
+                    print(s1.ljust(24), end='')
+                else:
+                    print(s1.ljust(25), end='')
+            else:
+                print(s1, end=' ')
+        elif count == 4:
+            s1 = str(list2[count])
+            print(s1, end=',')
+        elif count == 5:
+            s1 = str(list2[count-2])
+            print(s1)
+        s1 = ''
+
+
